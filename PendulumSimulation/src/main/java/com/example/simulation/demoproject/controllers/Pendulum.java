5aa3ee7cb6736b7a4cabe2647b68b17de3597535
Stanislava Germanova <114156419+StanislavaGermanovaa@users.noreply.github.com>
2025-02-23 17:06:33 +0200

    change group and fxml file
    adding rotation in SmartGroup and borderPane instead of anchorPane in fxml

--- PendulumSimulation/src/main/java/com/example/simulation/demoproject/controllers/Pendulum.java
+++ PendulumSimulation/src/main/java/com/example/simulation/demoproject/controllers/Pendulum.java
@@ -6,15 +6,15 @@ import javafx.scene.PerspectiveCamera;
 import javafx.scene.SubScene;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Sphere;
-import javafx.scene.transform.Rotate;
 
 public class Pendulum {
 
@@ -22,20 +22,12 @@ public class Pendulum {
     private PerspectiveCamera camera;
 
     @FXML
-    private AnchorPane anchorPane;
+    private BorderPane borderPane;
     @FXML
     private SubScene subScene;
 
     @FXML
-    private Box pendulumSide1;
-    @FXML
-    private Box pendulumSide2;
-
-    @FXML
-    private Box pendulumTop;
-
-    @FXML
-    private Box pendulumBase;
+    private Box pendulumSide1, pendulumSide2, pendulumTop, pendulumBase;
 
     @FXML
     private Circle circle;
@@ -47,17 +39,26 @@ public class Pendulum {
     private Sphere sphere;
 
     @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
     private Slider sliderLength, sliderMass, sliderAngle;
 
+    @FXML
+    private VBox VBox;
+//
+//    @FXML
+//    private TextField KeTextField, PeTextField;
+
+    private final double GRAVITY = 9.81; // ускорение на гравитацията
     private final SmartGroup group = new SmartGroup();
 
-    private double mousePosX, mousePosY;
-    private double mouseOldX, mouseOldY;
-    private final Rotate rotateX = new Rotate(0, Rotate.X_AXIS);
-    private final Rotate rotateY = new Rotate(0, Rotate.Y_AXIS);
+    // Стойности за махалото
+    private double length;  // Начална дължина
+    private double mass = 1.0;    // Начална маса
+    private double angle = 30;    // Начален ъгъл (в градуси)
 
     public void initialize() {
-
         prepareBox(pendulumSide1);
         prepareBox(pendulumSide2);
         prepareBox(pendulumTop);
@@ -65,21 +66,20 @@ public class Pendulum {
         prepareSphere(sphere);
 
         group.getChildren().addAll(pendulumSide1, pendulumSide2, pendulumTop, pendulumBase,circle,line,sphere);
-
-        group.getTransforms().addAll(rotateX, rotateY);
+        group.enableMouseRotation(anchorPane);
 
         subScene.setRoot(group);
         subScene.setCamera(camera);
 
-        initMouseControl(group, anchorPane);
     }
+
 
     private void prepareBox(Box box) {
         PhongMaterial material=new PhongMaterial();
         material.setDiffuseMap(new Image(getClass().getResourceAsStream("/images/wood.jpeg")));
         material.setBumpMap(new Image(getClass().getResourceAsStream("/images/woodBumpMap.jpeg")));
         material.setSpecularColor(Color.GRAY);
-
+        box.setVisible(true);
 
         box.setMaterial(material);
     }
@@ -88,38 +88,8 @@ public class Pendulum {
         PhongMaterial material=new PhongMaterial();
         material.setDiffuseMap(new Image(getClass().getResourceAsStream("/images/metal.jpeg")));
         material.setSpecularColor(Color.GRAY);
-
+        sphere.setVisible(true);
         sphere.setMaterial(material);
-    }
-
-    private void initMouseControl(SmartGroup group, AnchorPane scene) {
-        scene.setOnMousePressed(this::handleMousePressed);
-        scene.setOnMouseDragged(this::handleMouseDragged);
-
-        group.setOnScroll(scrollEvent -> {
-            double delta = scrollEvent.getDeltaY();
-            group.setTranslateZ(group.getTranslateZ() + delta);
-        });
-
-    }
-
-    private void handleMousePressed(MouseEvent event) {
-        mouseOldX = event.getSceneX();
-        mouseOldY = event.getSceneY();
-    }
-
-    private void handleMouseDragged(MouseEvent event) {
-        mousePosX = event.getSceneX();
-        mousePosY = event.getSceneY();
-
-        double deltaX = mousePosX - mouseOldX;
-        double deltaY = mousePosY - mouseOldY;
-
-        rotateY.setAngle(rotateY.getAngle() - deltaX * 0.3);
-        rotateX.setAngle(rotateX.getAngle() + deltaY * 0.3);
-
-        mouseOldX = mousePosX;
-        mouseOldY = mousePosY;
     }
 
 }
--- PendulumSimulation/src/main/java/com/example/simulation/demoproject/models/SmartGroup.java
+++ PendulumSimulation/src/main/java/com/example/simulation/demoproject/models/SmartGroup.java
@@ -1,23 +1,54 @@
 package com.example.simulation.demoproject.models;
 
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Transform;
 
 public class SmartGroup extends Group {
-    private Transform t = new Rotate();
 
+    private Rotate rotateX = new Rotate(0, Rotate.X_AXIS);
+    private Rotate rotateY = new Rotate(0, Rotate.Y_AXIS);
+    private Rotate rotateZ = new Rotate(0, Rotate.Z_AXIS);
+
+    public SmartGroup() {
+        // Добавяне на всички ротации към групата
+        this.getTransforms().addAll(rotateX, rotateY, rotateZ);
+    }
     public void rotateByX(int angle) {
-        Rotate r = new Rotate(angle, Rotate.X_AXIS);
-        t = t.createConcatenation(r);
-        this.getTransforms().clear();
-        this.getTransforms().add(t);
+        rotateX.setAngle(rotateX.getAngle() + angle);
     }
 
     public void rotateByY(int angle) {
-        Rotate r = new Rotate(angle, Rotate.Y_AXIS);
-        t = t.createConcatenation(r);
-        this.getTransforms().clear();
-        this.getTransforms().add(t);
+        rotateY.setAngle(rotateY.getAngle() + angle);
+    }
+    public void rotateByZ(int angle) {
+        rotateZ.setAngle(rotateZ.getAngle() + angle);
+    }
+
+    public void resetRotations() {
+        rotateX.setAngle(0);
+        rotateY.setAngle(0);
+        rotateZ.setAngle(0);
+    }
+
+    public void enableMouseRotation(Node node) {
+        final double[] mousePos = new double[2];
+
+        node.setOnMousePressed(event -> {
+            mousePos[0] = event.getSceneX();
+            mousePos[1] = event.getSceneY();
+        });
+
+        node.setOnMouseDragged(event -> {
+            double deltaX = event.getSceneX() - mousePos[0];
+            double deltaY = event.getSceneY() - mousePos[1];
+
+            rotateByY((int) (-deltaX * 0.3));
+
+            rotateByX((int) (deltaY * 0.3));
+
+            mousePos[0] = event.getSceneX();
+            mousePos[1] = event.getSceneY();
+        });
     }
 }
